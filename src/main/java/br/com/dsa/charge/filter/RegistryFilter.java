9c565a02672552ae895cf763a6183a142dbda454
Daniel Alves <daniel.saal@gmail.com>
2019-03-08 00:27:08 -0300

    Created filter for search

---
+++ src/main/java/br/com/dsa/charge/filter/RegistryFilter.java
@@ -0,0 +1,14 @@
+package br.com.dsa.charge.filter;
+
+public class RegistryFilter {
+
+	private String description;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
